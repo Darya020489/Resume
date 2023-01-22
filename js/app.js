684a9d08c24c55306624976030d4bc7220093039
Daria <daria7@tut.by>
2023-01-23 00:53:19 +0300

    added dropdown menu

--- js/app.js
+++ js/app.js
@@ -1,16 +1,10 @@
 const modal = document.querySelector(".modal");
 const avatar = document.querySelector(".main-info__avatar");
-// const span = document.querySelector(".modal__close");
 
 avatar.onclick = function () {
   modal.style.display = "block";
   document.body.classList.add("body_hidden");
 };
-
-// span.onclick = function () {
-//   modal.style.display = "none";
-//   document.body.classList.remove("body_hidden");
-// };
 
 window.onclick = function (event) {
   if (event.target === modal) {
